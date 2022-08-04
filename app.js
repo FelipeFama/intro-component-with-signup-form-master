2ea17274aa762bf0e53d7d471c3a31c9a5797264
Felipe <91050670+FelipeFama@users.noreply.github.com>
2022-08-04 00:24:08 -0300

    change in html, css and starting javascript.

---
+++ app.js
@@ -0,0 +1,24 @@
+//First Name
+const firstName = document.querySelector(".first-name");
+const firstNameLabel = document.querySelector(".firstname-label");
+const firstNameError = document.querySelector(".first-name-error");
+
+//Last name 
+const lastName = document.querySelector(".last-name");
+const lastNameLabel = document.querySelector(".lastname-label");
+const lastNameError = document.querySelector(".last-name-error");
+
+//Email
+const userEmail = document.querySelector(".user-email");
+const emailLabel = document.querySelector(".email-label");
+const emailError = document.querySelector(".user-email-error");
+
+//Password
+const userPassword = document.querySelector(".user-password");
+const passwordLabel = document.querySelector("password-label");
+const passwordError = document.querySelector(".user-password-error");
+
+const submitBtn = document.getElementById("submit-info");
+
+const successMsg = document.querySelector(".success-msg");
+
