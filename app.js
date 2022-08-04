124e9b56b529982c0ea3d5d2e0ad08ae6eea9e34
Felipe <91050670+FelipeFama@users.noreply.github.com>
2022-08-04 02:27:39 -0300

    Adding new features in javascript.

--- app.js
+++ app.js
@@ -15,10 +15,118 @@ const emailError = document.querySelector(".user-email-error");
 
 //Password
 const userPassword = document.querySelector(".user-password");
-const passwordLabel = document.querySelector("password-label");
+const passwordLabel = document.querySelector(".password-label");
 const passwordError = document.querySelector(".user-password-error");
 
 const submitBtn = document.getElementById("submit-info");
-
 const successMsg = document.querySelector(".success-msg");
 
+let emailValidation = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+let passwordValidation = /^[A-Za-z]\w{7,14}$/;
+
+let formError = function () {
+  if (firstName.value.length === 0) {
+    firstNameLabel.textContent = "First name cannot be empty";
+    firstName.style.borderColor = "red";
+    firstNameError.style.display = "inline";
+
+    setTimeout(() => {
+      firstNameLabel.textContent = "";
+      firstName.style.borderColor = "var(--grayish-blue)";
+      firstNameError.style.display = "none";
+    }, 2000);
+  }
+
+  if (lastName.value.length === 0) {
+    lastNameLabel.textContent = "Last name cannot be empty";
+    lastName.style.borderColor = "red";
+    lastNameError.style.display = "inline";
+
+    setTimeout(() => {
+      lastNameLabel.textContent = "";
+      lastName.style.borderColor = "var(--grayish-blue)";
+      lastNameError.style.display = "none";
+    }, 2000);
+
+    if (userEmail.value.length === 0) {
+      emailLabel.textContent = "Email cannot be empty";
+      userEmail.style.borderColor = "red";
+      emailError.style.display = "inline";
+
+      setTimeout(() => {
+        emailLabel.textContent = "";
+        userEmail.style.borderColor = "var(--grayish-blue)";
+        emailError.style.display = "none";
+      }, 2000);
+    }
+
+    if (userPassword.value.length === 0) {
+      passwordLabel.textContent = "Password cannot be empty";
+      userPassword.style.borderColor = "red";
+      passwordError.style.display = "inline";
+
+      setTimeout(() => {
+        passwordLabel.textContent = "";
+        userPassword.style.borderColor = "var(--grayish-blue)";
+        passwordError.style.display = "none";
+      }, 2000);
+    }
+  }
+
+  if (!userPassword.value.match(passwordValidation) && userPassword.value.length != 0) {
+    passwordLabel.textContent = "A password must be between 7-16 characters that starts with a letter(Only letters, numbers & underscore)";
+    userPassword.style.borderColor = "red";
+    passwordError.style.display = "inline";
+    setTimeout(() => {
+      passwordLabel.textContent = "";
+      userPassword.style.borderColor = "var(--grayish-blue)";
+      passwordError.style.display = "none";
+    }, 2000)
+  } else if (
+    firstName.value.length !== 0 &&
+    lastName.value.length !== 0 &&
+    userEmail.value.length !== 0 &&
+    userEmail.value.match(emailValidation) &&
+    userPassword.value.match(passwordValidation) &&
+    userPassword.value.length !== 0
+  ) {
+    successMsg.textContent = "Success! Please check your email";
+  } else {
+    console.log("err");
+  }
+}
+
+//Click with the mouse
+submitBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  formError();
+});
+
+//Click with Enter
+firstName.addEventListener("keyup", function (e) {
+  if (e.keycode === 13) {
+    e.preventDefault();
+    formError();
+  }
+});
+
+lastName.addEventListener("keyup", function (e) {
+  if (e.keycode === 13) {
+    e.preventDefault();
+    formError();
+  }
+});
+
+userEmail.addEventListener("keyup", function (e) {
+  if (e.keycode === 13) {
+    e.preventDefault();
+    formError();
+  }
+});
+
+userPassword.addEventListener("keyup", function (e) {
+  if (e.keycode === 13) {
+    e.preventDefault();
+    formError();
+  }
+});
